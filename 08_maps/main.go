1edf4dc42d8c664dafc9f18427f56b6394f105f8
jamestheengineer <jamestheengineer@gmail.com>
2023-03-15 18:47:53 -0400

    more maps

--- 08_maps/main.go
+++ 08_maps/main.go
@@ -5,10 +5,19 @@ import "fmt"
 func main() {
 	// Define a map
 	emails := make(map[string]string)
+	emails2 := map[string]string{"bob":"bob@gmaail.com", "sharon":"sharon@gmail.com"}
 
 	// Assign key values
 	emails["Bob"] = "bob@gmail.com"
 	emails["Sharon"] = "sharon@gmail.com"
+	emails["Mike"] = "mike@gmail.com"
 
 	fmt.Println(emails)
+	fmt.Println(emails["Bob"])
+	fmt.Println(len(emails))
+
+	delete(emails, "Bob")
+	fmt.Println(emails)
+
+	fmt.Println(emails2)
 }
