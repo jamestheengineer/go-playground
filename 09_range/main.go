c4324f4d85c47c33d7cde8ad45b9a9365f91b4e0
jamestheengineer <jamestheengineer@gmail.com>
2023-03-18 10:10:39 -0400

    Ranges

---
+++ 09_range/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	ids := []int{33,76,54,23,11,2}
+
+	// loop through ids
+	for i, id := range ids {
+		fmt.Printf("%d - ID: %d\n", i, id)
+	}
+
+	for _, id := range ids {
+		fmt.Printf("%d - ID: %d\n", id, id)
+	}
+
+	sum := 0
+	for _, id := range ids {
+		sum += id
+		fmt.Printf("%d - ID: %d\n", sum, id)
+	}
+
+	// Range with map
+	emails := map[string]string{"bob":"bob@gmaail.com", "sharon":"sharon@gmail.com"}
+
+	for k, v := range emails {
+		fmt.Printf("%s: %s\n", k, v)
+	}
+}
