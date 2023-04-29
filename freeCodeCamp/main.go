9c230ce5b8e20bf2d9d06aecd41998ec969460b2
jamestheengineer <jamestheengineer@gmail.com>
2023-04-29 11:12:57 -0400

    Variables

--- freeCodeCamp/main.go
+++ freeCodeCamp/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+var a = 42
+var (
+	actorName string = "Joe"
+	companion string = "Jack"
+	doctorNumber int = 3
+	season int = 11
+)
+
 func main() {
-	fmt.Println("Hello World")
+	var k int
+	k = 42
+	var i int = 42
+	j := 42
+
+	fmt.Println(i, j, k)
+	fmt.Printf("%v, %T\n", j, j)
 }
