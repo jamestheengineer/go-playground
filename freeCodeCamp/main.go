23af1519d30f56770ddb8548e787d0b00f63a1dc
jamestheengineer <jamestheengineer@gmail.com>
2023-05-21 18:19:59 -0400

    Ints

--- freeCodeCamp/main.go
+++ freeCodeCamp/main.go
@@ -6,7 +6,18 @@ func main() {
 	var n bool = true
 	fmt.Printf("%v, %T\n", n, n)
 
-	m := 2 == 1
-	fmt.Printf("%v, %T\n", m, n)
+	m := 42
+	fmt.Printf("%v, %T\n", m, m)
+
+	var p uint16 = 42
+	fmt.Printf("%v, %T\n", p, p)
+
+	a := 10
+	b := 3
+	fmt.Println(a + b)
+	fmt.Println(a - b)
+	fmt.Println(a * b)
+	fmt.Println(a / b)
+	fmt.Println(a % b)
 
 }
