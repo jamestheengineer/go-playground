a36d0d1547e8a3cbfab25f71990f9d8afb9c8e04
jamestheengineer <jamestheengineer@gmail.com>
2023-05-29 18:37:59 -0400

    Ints

--- freeCodeCamp/main.go
+++ freeCodeCamp/main.go
@@ -3,25 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	var n bool = true
-	fmt.Printf("%v, %T\n", n, n)
+	a := 10 // 1010
+	b := 3 // 0011
+	fmt.Println(a & b) // 0010
+	fmt.Println(a | b) // 1011
+	fmt.Println(a ^ b) // 1001
+	fmt.Println(a &^ b) // 0100
 
-	m := 42
-	fmt.Printf("%v, %T\n", m, m)
-
-	var p uint16 = 42
-	fmt.Printf("%v, %T\n", p, p)
-
-	a := 10
-	b := 3
-	fmt.Println(a + b)
-	fmt.Println(a - b)
-	fmt.Println(a * b)
-	fmt.Println(a / b)
-	fmt.Println(a % b)
-
-	var c int = 10
-	var d int8 = 3
-	fmt.Println(c + int(d))
-
+	c := 8
+	fmt.Println( c << 3) // 2^3 * 2^3
+	fmt.Println(c >> 3) // 2^3 / 2^3 = 2^0
 }
