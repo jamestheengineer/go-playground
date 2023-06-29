774e8ebb30475ffbced83c4119fbd8b35e3b43ed
jamestheengineer <jamestheengineer@gmail.com>
2023-06-28 20:01:03 -0400

    Complex numbers

--- freeCodeCamp/main.go
+++ freeCodeCamp/main.go
@@ -3,19 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	a := 10 // 1010
-	b := 3 // 0011
-	fmt.Println(a & b) // 0010
-	fmt.Println(a | b) // 1011
-	fmt.Println(a ^ b) // 1001
-	fmt.Println(a &^ b) // 0100
+	d := 1 + 2i
+	var e = 2 + 5.2i
 
-	c := 8
-	fmt.Println( c << 3) // 2^3 * 2^3
-	fmt.Println(c >> 3) // 2^3 / 2^3 = 2^0
-
-	n := 3.14
-	n = 13.7e72
-	n = 2.1E14
-	fmt.Printf("%v, %T\n", n, n)
+	fmt.Println(d + e)
+	fmt.Println(d - e)
+	fmt.Println(d * e)
+	fmt.Println(d / e)
+	
 }
